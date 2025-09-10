cbae9790fecc201258ff0dfb15f2de8d8a05ed03
amonreis <ammonreis@gmail.com>
2025-09-09 22:01:17 -0600

    Finished programming exercise 3

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Write a program that determines the letter grade for a course according to the grade percentage:
+        Console.WriteLine("What is your grade percentage?");
+        Console.Write(">> ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
+
+        // Determine the letter grade according to the grade percentage
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        // Determine the sign
+        if (grade % 10 >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (grade % 10 <= 3)
+        {
+            sign = "-";
+        }
+
+        // Handle A+, F+, and F- cases
+        if (grade >= 97)
+        {
+            sign = "";
+        }
+
+        if (grade < 60)
+        {
+            sign = "";
+        }
+
+        // Display the letter grade along with a message according to the grade percentage
+        Console.WriteLine($"Your letter grade is {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You have passed the course! Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed the course! Tutoring is available for an additional fee.");
+        }
     }
 }
